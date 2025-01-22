66f58a3e3bb70aad35c550ac1edc0c3f13312eb7
Olena Datso <elena.shcheglova@gmail.com>
2025-01-21 18:05:24 -0700

    refactor: remove unnecessery code

--- jammming/src/App.tsx
+++ jammming/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import ResultsContainer from './components/resultContainer/ResultsContainer';
+import Searchbar from './components/searchbar/Searchbar';
+import MyListContainer from './components/myListContainer/MyListContainer';
+import { TrackProvider } from './components/context/TracksContext';
+import SpotifyLogin from './components/api/auth';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <TrackProvider>
+      <div className="container grid grid-cols-3 gap-4 md:grid-cols-5 lg:grid-cols-8">
+        <Searchbar className="col-span-full lg:col-span-4 lg:col-start-3 mt-10 p-4"></Searchbar>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="container grid grid-cols-3 gap-4 md:grid-cols-5 lg:grid-cols-8">
+        <div className="col-span-full lg:col-span-4 ">
+          <ResultsContainer></ResultsContainer>
+        </div>
+        <div className="col-span-full lg:col-span-4 ">
+          <MyListContainer></MyListContainer>
+        </div>
+        <SpotifyLogin />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </TrackProvider>
+  );
 }
 
-export default App
+export default App;
